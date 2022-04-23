7c0c952a2807e717b6450d3bd551c9f2dc150e9e
Shubham Patil <shubhampatilsd@gmail.com>
2022-04-22 23:34:19 -0700

    skills

---
+++ components/Skills.tsx
@@ -0,0 +1,7 @@
+export const Skills = () => {
+  return (
+    <div>
+      <h3 className="text-[#00b894]">Stuff i know</h3>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,18 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { Hero } from "../components/Hero";
 import { Navbar } from "../components/Navbar";
+import { Skills } from "../components/Skills";
 
 const Home: NextPage = () => {
   return (
     <>
       <Navbar />
-      <Hero />
+      <div className="flex md:justify-center p-12 md:p-4">
+        <div className="space-y-16">
+          <Hero />
+          <Skills />
+        </div>
+      </div>
     </>
   );
 };

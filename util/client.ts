c6f7ecefe28af72174f2b8804194999575683644
Shubham Patil <shubhampatilsd@gmail.com>
2022-06-18 17:42:55 -0700

    add axios instance

--- util/client.ts
+++ util/client.ts
@@ -3,7 +3,7 @@ import imageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID || "7ok6rxeh",
+  projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET, // or the name you chose in step 1
   useCdn: true,
 });

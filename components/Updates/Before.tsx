fbdf238e4bd2b675e1a8ad7b3f1a7d0200a823d8
Shubham Patil <shubhampatilsd@gmail.com>
2022-08-15 20:18:27 -0700

    added toorcamp

--- components/Updates/Before.tsx
+++ components/Updates/Before.tsx
@@ -3,6 +3,7 @@ import ReactMarkdown from "react-markdown";
 const text = `
 In the past, I have:
 
+- Gave a [talk](https://vimeo.com/showcase/9681417/video/730487346#t=1h7m30s) at [Toorcamp](https://toorcamp.toorcon.net/) (a big 5 day cybersecurity camping event with 600+ people) on how to generate a cool looking sketch effect of yourself (and print it, like a polaroid) with Python! [Kai Devrim](https://devrim.tech/) was also the co-host of the talk.
 - Interned at [CodeDay Labs](labs.codeday.org) and built [Disco Wave](https://showcase.codeday.org/project/ckqtyhfmo91620210qu4j6ddiak)
 - Won 3rd place at [LancerHacks](https://www.lancerhacks.com/) with a web application called [GeoChattr](https://devpost.com/software/geochattr)
 - Built a website for an organization called [Edumango](https://edumango.vercel.app), and taught an introductory Java course to middle schoolers

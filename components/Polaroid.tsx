78a03d0a4d9232379ad08f984c4ebbb216f6c60b
Shubham Patil <shubhampatilsd@gmail.com>
2023-06-11 16:32:12 -0700

    finish styling polaroid

--- components/Polaroid.tsx
+++ components/Polaroid.tsx
@@ -1,18 +1,13 @@
 export const Polaroid = () => {
   return (
-    <div className="p-4 bg-white w-72 shadow-2xl border border-grey space-y-4">
+    <div className="p-4 rotate-[4deg] transform h-fit bg-white w-72 shadow-2xl rounded-lg border border-grey space-y-3">
       <img
         src="https://avatars.githubusercontent.com/u/55328098?v=4"
-        className="rounded-sm"
+        className="rounded-md"
       />
 
       <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
-          dicta neque repellendus pariatur ipsam consequatur eum mollitia, et
-          voluptatem omnis quos odio sequi maiores reprehenderit temporibus
-          excepturi impedit expedita. Nemo.
-        </p>
+        <p className="marker">me :)</p>
       </div>
     </div>
   );
